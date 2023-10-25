8d04fbf6d6d88e385c0792b1c4b0da1c83538629
MarcusVMC <marcus_vmc@outlook.com>
2023-10-25 20:31:29 -0300

    Desafio 2 Felipão DIO Finalizado

---
+++ index.js
@@ -0,0 +1,37 @@
+function calculadoraRank (vitorias, derrotas){
+	let saldoVitorias = vitorias - derrotas
+	let nivel = ""
+    const mensagem = "O herói tem saldo de " + saldoVitorias + " vitórias e está no nível "
+	
+    if(saldoVitorias <= 10){
+        nivel= "Ferro"
+        console.log (mensagem + nivel)
+    }
+    else if (saldoVitorias >= 11 && saldoVitorias<= 20){
+        nivel = "Bronze"
+        console.log (mensagem + nivel)
+
+    }
+    else if (saldoVitorias >= 21 && saldoVitorias <=50 ){
+        nivel = "Prata"
+        console.log (mensagem + nivel)
+    }
+    else if (saldoVitorias >= 51 && saldoVitorias <=80 ){
+        nivel = "Ouro"
+        console.log (mensagem + nivel)
+    }
+    else if (saldoVitorias >= 81 && saldoVitorias <=90 ){
+        nivel = "Diamante"
+        console.log (mensagem + nivel)
+    }
+    else if (saldoVitorias >= 91 && saldoVitorias <=100 ){
+        nivel = "Lendário"
+        console.log (mensagem + nivel)
+    }
+    else{
+        nivel = "imortal"
+        console.log (mensagem + nivel)
+    }
+        
+}
+calculadoraRank (102,0)
