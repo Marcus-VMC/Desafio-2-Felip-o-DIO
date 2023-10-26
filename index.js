9b967750c898896984375a4b61158c3f2167a8d7
Marcus-VMC <148163830+Marcus-VMC@users.noreply.github.com>
2023-10-25 21:37:29 -0300

    Update index.js
    Melhorias

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 function calculadoraRank (vitorias, derrotas){
 	return vitorias - derrotas
 }
-let saldoVitorias = calculadoraRank (900,0)
+let saldoVitorias = calculadoraRank (103,0)
 
 const mensagem = "O herói tem saldo de " + saldoVitorias + " vitórias e está no nível "
    
 if(saldoVitorias <= 10){
         nivel= "Ferro"
-        console.log (mensagem + nivel)
     }
     else if (saldoVitorias >= 11 && saldoVitorias<= 20){
         nivel = "Bronze"
-        console.log (mensagem + nivel)
     }
     else if (saldoVitorias >= 21 && saldoVitorias <=50 ){
         nivel = "Prata"
-        console.log (mensagem + nivel)
     }
     else if (saldoVitorias >= 51 && saldoVitorias <=80 ){
         nivel = "Ouro"
-        console.log (mensagem + nivel)
     }
     else if (saldoVitorias >= 81 && saldoVitorias <=90 ){
         nivel = "Diamante"
-        console.log (mensagem + nivel)
     }
     else if (saldoVitorias >= 91 && saldoVitorias <=100 ){
         nivel = "Lendário"
-        console.log (mensagem + nivel)
     }
     else{
         nivel = "imortal"
-        console.log (mensagem + nivel)
     }
+    console.log (mensagem + nivel)
